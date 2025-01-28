113ca4f0fd7e1ac0387a298a227e2f1abce3ae10
Vivian Kesse <vivian.kessy1@gmail.com>
2025-01-28 17:58:14 -0300

    first commit

---
+++ models/User.js
@@ -0,0 +1,19 @@
+//todos os tipos de dados que o banco de dados possui
+const {DataTypes} = require('sequelize');
+const db = require('../db/conn')
+
+const User = db.define('User', {
+  name: {
+    type:DataTypes.STRING,
+    allowNull: false
+  },
+  occupation: {
+    type: DataTypes.STRING,
+    required: true
+  },
+  newsletter: {
+    type: DataTypes.BOOLEAN,
+  }
+})
+
+module.exports = User
